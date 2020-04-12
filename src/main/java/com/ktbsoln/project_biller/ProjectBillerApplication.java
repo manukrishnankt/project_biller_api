4b0cb17bbe272a3963f47b231e1fb6229e70b887
manukrishnankt <manukrishnankt@gmail.com>
2020-04-12 16:58:29 +0530

    intial

---
+++ src/main/java/com/ktbsoln/project_biller/ProjectBillerApplication.java
@@ -0,0 +1,13 @@
+package com.ktbsoln.project_biller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBillerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktbsoln/project_biller/ProjectBillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktbsoln.project_biller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
